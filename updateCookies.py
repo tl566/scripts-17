8f40ad9057e7c9d0d83070f96b2b573182a8b1e3
xyh <977732241@qq.com>
2021-09-03 14:43:52 +0800

    1

--- updateCookies.py
+++ updateCookies.py
@@ -26,21 +26,21 @@ def gettimestamp():
 
 
 def login(username, password):
-    url = "http://127.0.0.1:5700/api/login?t=%s" % gettimestamp()
+    url = "http://127.0.0.1:5704/api/login?t=%s" % gettimestamp()
     data = {"username": username, "password": password}
     r = s.post(url, data)
     s.headers.update({"authorization": "Bearer " + json.loads(r.text)["data"]["token"]})
 
 
 def getitem(searchValue):
-    url = "http://127.0.0.1:5700/api/envs?searchValue=%s&t=%s" % (searchValue, gettimestamp())
+    url = "http://127.0.0.1:5704/api/envs?searchValue=%s&t=%s" % (searchValue, gettimestamp())
     r = s.get(url)
     item = json.loads(r.text)["data"]
     return item
 
 
 def getckitem(searchValue, value):
-    url = "http://127.0.0.1:5700/api/envs?searchValue=%s&t=%s" % (searchValue, gettimestamp())
+    url = "http://127.0.0.1:5704/api/envs?searchValue=%s&t=%s" % (searchValue, gettimestamp())
     r = s.get(url)
     for i in json.loads(r.text)["data"]:
         if value in i["value"]:
@@ -101,7 +101,7 @@ def checkcookie(cookies):
 
 
 def update(text, qlid):
-    url = "http://127.0.0.1:5700/api/envs?t=%s" % gettimestamp()
+    url = "http://127.0.0.1:5704/api/envs?t=%s" % gettimestamp()
     s.headers.update({"Content-Type": "application/json;charset=UTF-8"})
     data = {
         "name": "JD_COOKIE",
@@ -116,7 +116,7 @@ def update(text, qlid):
 
 
 def insert(text):
-    url = "http://127.0.0.1:5700/api/envs?t=%s" % gettimestamp()
+    url = "http://127.0.0.1:5704/api/envs?t=%s" % gettimestamp()
     s.headers.update({"Content-Type": "application/json;charset=UTF-8"})
     data = []
     data_json = {
