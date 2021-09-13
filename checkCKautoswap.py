a218724d9898b28036e96e5bfc4436aefee5d203
xyh <977732241@qq.com>
2021-09-13 14:16:44 +0800

    测试

--- checkCKautoswap.py
+++ checkCKautoswap.py
@@ -1,7 +1,3 @@
-#const exec = require('child_process').exec;
-
-#if(text.indexOf("失效")! =-1){
-	exec('task raw_master_de.py', function(error, stdout, stderr){
-		console.log(error, stdout, stderr)
-	})
-#}
+exec('task raw_master_de.py', function(error, stdout, stderr){
+	console.log(error, stdout, stderr)
+})
