432b6fabbaf0b6a979dd67357aa86fa618c96e31
xyh <977732241@qq.com>
2021-09-13 14:09:34 +0800

    检测ck过期自动运行转换脚本

--- checkCKautoswap.py
+++ checkCKautoswap.py
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec;
 
-if(text.indexOf("cookie已失效")! =-1){
+if(text.indexOf("失效")! =-1){
 	exec('task raw_master_de.py', function(error, stdout, stderr){
 		console.log(error, stdout, stderr)
 	})
