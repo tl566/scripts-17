2e66cbb7f7f9df2718ddfca7b4afff265db6726c
xyh <977732241@qq.com>
2021-09-14 08:34:46 +0800

    依赖

--- jdCookie.js
+++ jdCookie.js
@@ -5,7 +5,7 @@
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
   '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
-  '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
+  '',//账号二ck,例:pt_key=XXX;pt_pin=XXX; 如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
 if (process.env.JD_COOKIE) {
