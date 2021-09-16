6ee5c79fe382fefcf97b35925e3b87c1c9025bd4
xyh <977732241@qq.com>
2021-09-16 15:53:09 +0800

    调试

--- checkCk.py
+++ checkCk.py
@@ -46,5 +46,6 @@ if __name__ == '__main__':
     s = requests.session()
     # cookies = getitem("JD_COOKIE")
     cookies = os.environ['JD_COOKIE']
+    cookies = cookies.split('&')
     for i in cookies:
         check_ck(cookies[i])
