4735f35ae47142bfc88788d0d8b071933d052ff7
xyh <977732241@qq.com>
2021-09-17 11:45:41 +0800

    自用检测ck

--- checkCk.py
+++ checkCk.py
@@ -64,7 +64,9 @@ def check_ck(ck):
     headers = {'Cookie': ck, 'Referer': 'https://home.m.jd.com/myJd/home.action',
                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Mobile/15E148 Safari/604.1', }
     res = requests.get(url=url, headers=headers, verify=False, timeout=30)
-    pin = ck.split(";")[1]
+    pin = ck.split(";")[1][7:]
+    print(pin)
+    # pin = ck.match(/pt_pin=([^; ]+)(?=;?)/)
     if res.status_code == 200:
         code = int(json.loads(res.text)['retcode'])
         if code == 0:
